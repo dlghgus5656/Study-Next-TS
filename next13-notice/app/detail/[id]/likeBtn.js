22b60c7485b7660aa7873ad54f0bdf62e089b87f
dlghgus5656 <dlghgus5656@gmail.com>
2023-07-11 17:31:18 +0900

    좋아요 수 에러 해결 CSR에서 async 사용 x

--- next13-notice/app/detail/[id]/likeBtn.js
+++ next13-notice/app/detail/[id]/likeBtn.js
@@ -2,23 +2,19 @@
 
 import { useEffect, useState } from "react";
 
-export default async function LikeBtn(postid) {
+export default function LikeBtn(postid) {
   const [likeCount, setLikeCount] = useState(0);
+  const [state, setState] = useState(false);
   useEffect(() => {
-    // 서버에서 댓글 가져오기
-    // 1. 컴포넌트 로드시 서버에서 댓글 가져옴
-    // 2. 가져온 데이터를 state에 저장해둠
-    // 3. state를 html에 꽂아서 보여줌
-
-    // 아래 GET요청시 데이터 함께 보내려면 크게 두가지 방법이 있다.
-    // URL parameter 과 query string 아래에선 조금 더 간단한 query string방법을 사용한다.
     fetch("/api/post/checkLike?id=" + postid.postid)
       .then((r) => r.json())
       .then((data) => {
         setLikeCount(data.likesCount);
+        console.log(data.likesCount, "data.likesCount");
       });
-  }, []);
-  console.log(likeCount, "likeCount");
+  }, [likeCount, state]);
+  console.log(state, "state");
+  //
   return (
     <div>
       <button
@@ -38,6 +34,7 @@ export default async function LikeBtn(postid) {
             })
             .then((data) => {
               // data는 서버에서 응답받은 JSON 데이터입니다.
+              setState(true);
               alert(data);
             })
             .catch((error) => {
--- next13-notice/pages/api/post/checkLike.js
+++ next13-notice/pages/api/post/checkLike.js
@@ -1,9 +1,19 @@
 import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청, 응답) {
+  let session = await getServerSession(요청, 응답, authOptions); // api에서 사용시 요청, 응답도 넣어주어야 한다./ 로그인한 사용자 정보가 필요할때 사용?
+  console.log(session.user.email, "session");
   console.log(요청.query.id, "요청 쿼리 좋아요");
+  const likeCheck = await db.collection("likespost").findOne({});
+  //     if (session === null) {
+  //     return 응답.status(500).json("좋아요 실패: 로그인 안함");
+  //   } else {
+
+  //   }
   try {
     // 좋아요 내역을 저장하는 컬렉션을 생성합니다.
     const db = (await connectDB).db("nextjsnotice");
