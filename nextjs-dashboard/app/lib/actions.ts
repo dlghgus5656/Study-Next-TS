d49a5383dc45c1d9a5f501d5533ca451f4cd929b
dlghgus5656 <dlghgus5656@gmail.com>
2024-03-15 13:34:25 +0900

    useFormState서버 작업을 사용하여 서버측 유효성 검사를 구현하는 방법과 접근성을 염두에 두고 후크를 사용하여 양식 오류를 표시하는 방법을 살펴보겠습니다

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -6,25 +6,46 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+// customerId- Zod는 string 유형을 예상하여 고객 필드가 비어 있으면 이미 오류를 발생시킵니다 . 하지만 사용자가 고객을 선택하지 않으면 친근한 메시지를 추가해 보겠습니다.
+// amount- 금액 유형을 에서 로 강제 변환하므로 string문자열 number이 비어 있으면 기본값은 0이 됩니다. Zod에게 우리는 항상 gt().함수를 사용하여 0보다 큰 양을 원한다고 말합시다.
+// status- Zod는 "보류 중" 또는 "지불됨"을 예상하므로 상태 필드가 비어 있으면 이미 오류를 발생시킵니다. 사용자가 상태를 선택하지 않은 경우에도 친근한 메시지를 추가해 보겠습니다.
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   //   const rawFormData = {
-  const { customerId, amount, status } = CreateInvoice.parse({
+  // Validate form fields using Zod
+  //safeParse()success또는 필드 를 포함하는 객체를 반환합니다 error. 이렇게 하면 이 논리를 try/catch 블록 안에 넣지 않고도 유효성 검사를 보다 원활하게 처리하는 데 도움이 됩니다.
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    console.log(validatedFields, 'validatedFields');
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   // 값을 센트 단위로 저장
   // 일반적으로 JavaScript 부동 소수점 오류를 제거하고 정확성을 높이기 위해 데이터베이스에 금전적 가치를 센트 단위로 저장하는 것이 좋습니다.
   // 금액을 센트로 변환해 보겠습니다. - amountInCents
@@ -59,12 +80,24 @@ export async function createInvoice(formData: FormData) {
 // 변수를 SQL 쿼리에 전달합니다.
 // revalidatePath클라이언트 캐시를 지우고 새 서버 요청을 하기 위해 호출합니다 .
 // redirect사용자를 청구서 페이지로 리디렉션하기 위해 호출합니다 .
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   // 값을 센트 단위로 저장
   // 일반적으로 JavaScript 부동 소수점 오류를 제거하고 정확성을 높이기 위해 데이터베이스에 금전적 가치를 센트 단위로 저장하는 것이 좋습니다.
